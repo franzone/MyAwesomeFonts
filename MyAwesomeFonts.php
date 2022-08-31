96b2eded7948d8bdfbb7f752aadba99c80b1d2d9
Jonathan Franzone <jonathan@franzone.com>
2022-08-30 21:15:12 -0500

    Fixing exception

--- MyAwesomeFonts.php
+++ MyAwesomeFonts.php
@@ -13,7 +13,7 @@ Author URI: https://about.franzone.come
 */
 
 public function myawesomefonts_enqueue_assets() {
-    wp_enqueue_style( 'font-awesome-5', plugins_url( 'assets/fontawesome/css/all.min.css', __FILE__ ), [], $this->version );
-    wp_enqueue_style( 'font-awesome-4-shims', plugins_url( 'assets/fontawesome/css/v4-shims.min.css', __FILE__ ), [], $this->version );
+    wp_enqueue_style( 'font-awesome-5', plugins_url( 'assets/fontawesome/css/all.min.css', __FILE__ ), [], "5.1.3" );
+    wp_enqueue_style( 'font-awesome-4-shims', plugins_url( 'assets/fontawesome/css/v4-shims.min.css', __FILE__ ), [], "5.1.3" );
 }
 add_action('wp_enqueue_scripts', 'myawesomefonts_enqueue_assets');
